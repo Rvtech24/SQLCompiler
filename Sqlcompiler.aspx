93f257b4c35112fd02b52349e84892f1e31877c8
Rvtech24 <135835091+Rvtech24@users.noreply.github.com>
2023-07-01 11:24:39 +0530

    Add files via upload

---
+++ Sqlcompiler.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Sqlcompiler.aspx.cs" Inherits="Sqlcompiler" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 865px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table class="auto-style1">
+            <tr>
+                <td colspan="2">
+                    <asp:TextBox ID="tbxquerypane" runat="server" Rows="15" TextMode="MultiLine" Width="100%" Font-Bold="False"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <asp:TextBox ID="tbxrespane" runat="server" ReadOnly="True" Width="100%"></asp:TextBox>
+                </td>
+                <td align="center">
+                    <asp:Button ID="btnrun" runat="server" Text="Run" OnClick="btnrun_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:GridView ID="gvresultpane" runat="server" Width="100%" AllowPaging="True" OnPageIndexChanging="gvresultpane_PageIndexChanging" OnSelectedIndexChanged="gvresultpane_SelectedIndexChanged" PageSize="4">
+                    </asp:GridView>
+                </td>
+            </tr>
+        </table>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ Sqlcompiler.aspx.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class Sqlcompiler : System.Web.UI.Page
+{
+    SqlConnection sqlcn;
+    SqlCommand sqlcmd;
+    SqlDataAdapter sqlda;
+    DataSet ds;
+
+    static string ddLQry; 
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        tbxrespane.Text = null;
+        try
+        {
+            string s = ConfigurationManager.ConnectionStrings["rvtechdbConnectionString"].ToString();
+            sqlcn = new SqlConnection(s);
+
+        }
+        catch(Exception ex)
+        {
+            tbxrespane.Text = ex.Message;
+        }
+    }
+
+   
+    protected void gvresultpane_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void gvresultpane_PageIndexChanging(object sender, GridViewPageEventArgs e)
+    {
+        gvresultpane.PageIndex = e.NewPageIndex;
+        uRefreshGridView();
+    }
+    protected void btnrun_Click(object sender, EventArgs e)
+    {
+        uRunQuery();
+    }
+    public void uRunQuery()
+    {
+        tbxrespane.Text = null;
+        gvresultpane.EmptyDataText = null;
+        gvresultpane.DataBind();
+
+        string qry = tbxquerypane.Text;
+        try
+        {
+            if(qry.ToLower().StartsWith("select"))
+            {
+                ddLQry = qry;
+                uRefreshGridView();
+                return;
+            }
+            sqlcn.Open();
+            sqlcmd = new SqlCommand(qry, sqlcn);
+
+            int i = sqlcmd.ExecuteNonQuery();
+            if(i>0)
+            {
+                tbxrespane.Text = i + " Row(s) Affected";
+            }
+            else
+            {
+                tbxrespane.Text = "Command(s) Complete Successfully";
+            }
+            sqlcn.Close();
+        }
+        catch(Exception e)
+        {
+            tbxrespane.Text = e.Message;
+        }
+    }
+    public void uRefreshGridView()
+    {
+        tbxrespane.Text = null;
+        gvresultpane.DataSource = null;
+        gvresultpane.DataBind();
+        try
+        {
+            sqlcn.Open();
+            sqlcmd = new SqlCommand(ddLQry, sqlcn);
+            sqlda = new SqlDataAdapter(sqlcmd);
+
+            ds = new DataSet();
+            sqlda.Fill(ds);
+            sqlcn.Close();
+
+            gvresultpane.DataSource = ds;
+            gvresultpane.DataBind();
+            tbxrespane.Text= "Command(s) Complete Successfully";
+        }
+        catch(Exception e)
+        {
+
+            tbxrespane.Text = e.Message;
+        }
+    }
+
+}
